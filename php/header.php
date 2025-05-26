c8c03bdd9545ae711d66cd3240dd6d6a72351a83
Natalia Cuellas <nataliagcuellas@gmail.com>
2025-05-25 23:05:34 -0300

    testing favicon 2

--- php/header.php
+++ php/header.php
@@ -1,17 +1,13 @@
-<head>¡
+<head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
    <title>FOX Studio - Agencia Digital</title>
    <meta name="keywords" content="creative, agency, portfolio, web designer, web developer, desarrollador web, programador, webs, agencia, creativa, portfolio">
    <meta name="description" content="Fox Studio - Agencia Digital">
-   <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-   <link rel="stylesheet" href="assets/vendor/glyyphter/css/xpovio.css">
-   <link rel="stylesheet" href="assets/vendor/font-awesome/css/all.css">
-   <link rel="stylesheet" href="assets/vendor/nice-select/css/nice-select.css">
-   <link rel="stylesheet" href="assets/vendor/magnific-popup/css/magnific-popup.css">
-   <link rel="stylesheet" href="assets/vendor/slick/css/slick.css">
-   <link rel="stylesheet" href="assets/css/main.min.css">
+
+   
    <!-- Favicon-->
    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
@@ -19,6 +15,16 @@
    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
    <link rel="manifest" href="/site.webmanifest" />
+
+   
+   <!-- CSS-->
+   <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/vendor/glyyphter/css/xpovio.css">
+   <link rel="stylesheet" href="assets/vendor/font-awesome/css/all.css">
+   <link rel="stylesheet" href="assets/vendor/nice-select/css/nice-select.css">
+   <link rel="stylesheet" href="assets/vendor/magnific-popup/css/magnific-popup.css">
+   <link rel="stylesheet" href="assets/vendor/slick/css/slick.css">
+   <link rel="stylesheet" href="assets/css/main.min.css">
 </head>
 
 <body>
